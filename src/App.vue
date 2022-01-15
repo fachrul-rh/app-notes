10545f46e0bd92698a4e71e5c343c05f70495e31
fachrul-rh <fachrulstroid@gmail.com>
2022-01-15 11:06:21 +0700

    menampilkan data ketika di klik

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,171 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="kiri">
+      <div class="logo">
+        <a href="#">
+          <h2>Farul</h2>
+          <span>Notes</span>
+        </a>
+      </div>
+      <div class="frame-notes">
+        <button @click="newNote" class="bg-success btn btn-new-note">+ Note Baru</button>
+
+        <ListNotes :propNotes="notes" :propEditNote="editNote" />
+      </div>
+
+      <!-- List -->
+    </div>
+    <div class="kanan">
+      <!-- Form -->
+      <FormNotes :propSaveNote="saveNote" :propDataForm="dataform" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ListNotes from './components/listNotes.vue';
+import FormNotes from './components/formNote.vue';
 
 export default {
   name: 'App',
+  data: function () {
+    return {
+      dataform: {},
+      notes: [
+        { id: 1, title: 'Wegodev', description: 'Ini isi wegodev' },
+        { id: 2, title: 'Super User', description: 'Ini adalah kamu' },
+      ],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    ListNotes,
+    FormNotes,
+  },
+  methods: {
+    editNote(id) {
+      // console.log('app Vue' + id);
+      this.dataform = this.notes.find((note) => note.id === id);
+      console.log(this.dataform);
+    },
+    newNote() {
+      // kosong dulu
+    },
+    saveNote(title, description) {
+      let newNote = { title: title, description: description };
+      this.notes.push(newNote);
+    },
+  },
+};
 </script>
 
 <style>
+body {
+  margin: 0px;
+  overflow: hidden;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  font-family: 'Avenir', Arial, Helvetica, sans-serif;
   color: #2c3e50;
-  margin-top: 60px;
+  padding: 0px;
+  display: flex;
+  width: 100%;
+}
+
+.kiri {
+  width: 400px;
+  background: #f7f7f7;
+  color: #616161;
+}
+
+.logo {
+  padding: 25px 15px;
+  border-bottom: 1px solid gainsboro;
+}
+
+.logo a {
+  text-decoration: none;
+}
+
+.logo a h2 {
+  margin: 0;
+  display: inline;
+  margin-right: 5px;
+  text-transform: capitalize;
+  color: #757575;
+}
+
+.logo a span {
+  font-size: 12px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: #139e5f;
+}
+
+.frame-notes {
+  overflow-y: scroll;
+  overflow-x: hidden;
+  height: 85vh;
+}
+
+.bg-success {
+  background: #219a63;
+  color: white;
+  outline: none;
+}
+
+.bg-success:hover {
+  background: #24b774;
+  color: white;
+}
+.bg-danger {
+  background: #b50000;
+  color: white;
+}
+.bg-danger:hover {
+  background: #c50000;
+}
+.btn {
+  border: none;
+  font-size: 12px;
+  text-align: center;
+  letter-spacing: 1px;
+  cursor: pointer;
+  border-radius: 2px;
+  padding: 7px 25px;
+  outline: none;
+}
+.btn-new-note {
+  width: 90%;
+  padding: 12px 10px;
+  margin: 10px 15px;
+  text-align: left !important;
+}
+
+.kanan {
+  width: 100%;
+  overflow-y: scroll;
+  height: 100vh;
+  border-left: 1px solid gainsboro;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f7f7f7;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
 }
 </style>
---
+++ src/components/formNote.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="formNotes">
+    <form @submit="submitNote">
+      <div class="menu">
+        <button type="button" class="bg-danger btn btn-delete">Delete</button>
+        <button type="submit" class="bg-success btn">Save</button>
+      </div>
+
+      <div class="content">
+        <input type="text" class="text" placeholder="Title" v-model="title" />
+        <textarea class="text textarea" placeholder="Tuliskan acara kamu..." v-model="description"></textarea>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'formNotes',
+  props: {
+    propSaveNote: {
+      type: Function,
+    },
+    propDataForm: {
+      type: Object,
+    },
+  },
+  data: function () {
+    return {
+      title: '',
+      description: '',
+    };
+  },
+  methods: {
+    submitNote(e) {
+      e.preventDefault();
+      this.propSaveNote(this.title, this.description);
+    },
+  },
+  watch: {
+    propDataForm: function (note) {
+      (this.title = note.title), (this.description = note.description);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.menu {
+  background: #f7f7f7;
+  padding: 10px 25px;
+  margin-bottom: 25px;
+  text-align: right;
+  border-bottom: 1px solid #e8e6e6;
+}
+.btn-delete {
+  margin-right: 10px;
+}
+.content {
+  padding: 0px 25px;
+}
+.text {
+  display: block;
+  width: 100%;
+  padding: 0px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #2c3e50;
+  border: none;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  outline: none;
+}
+.textarea {
+  min-height: 350px;
+  font-size: 15px;
+  font-weight: lighter;
+  line-height: 30px;
+}
+.loader {
+  vertical-align: middle;
+}
+</style>
